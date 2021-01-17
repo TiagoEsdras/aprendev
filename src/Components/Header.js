bad2b96d17143755ba24bdca9ee2cf775b84b31c
TIAGO <tiagoesdrasvieira@gmail.com>
2021-01-16 21:43:02 -0300

    Criando Header e menu

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          logo
+        </div>
+        <nav>
+          <li>Contato</li>
+          <li>Registre-se</li>
+          <li>Login</li>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Header;
