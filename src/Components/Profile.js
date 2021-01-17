49d5fa51fd59f09802034c17fbc6860560e6bf05
TIAGO <tiagoesdrasvieira@gmail.com>
2021-01-17 14:31:26 -0300

    Estilizando secao perfil

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,61 +1,64 @@
 import { Component } from 'react';
+import estudante from '../images/Group 8.svg';
+import empresa from '../images/Group 6.svg';
+import ong from '../images/g11.svg';
 
 class Profile extends Component {
   render() {
-    const { handlerClickONG, handlerClickCompany, handlerClickStudent} = this.props;
+    const { handlerClickONG, handlerClickCompany, handlerClickStudent } = this.props;
     return (
-      <div>
+      <div class="profile-section">
         <div>
-          <h1>Escolha seu perfil</h1>
+          <h1 class="profile-title">Escolha seu perfil</h1>
         </div>
-        <div>
-          <div>
+        <div class="profile-cards">
+          <div class="card">
             <div>
-              <h2>Estudante</h2>
+              <h2 class="card-profile-title studant">Estudante</h2>
+            </div>
+            <div class="img-div">
+              <img src={estudante} alt="estudante" />
             </div>
             <div>
-              imagem
-            </div>
-            <div>
-              <p>
+              <p class="profile-text">
                 Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
               </p>
               <div>
-                <button onClick={ () => handlerClickStudent() }>Inscreva-se agora</button>
-              </div>
-            </div>
-          </div>
-          
-          <div>
-            <div>
-              <h2>ONG</h2>
-            </div>
-            <div>
-              imagem
-            </div>
-            <div>
-              <p>
-                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
-              </p>
-              <div>
-                <button onClick={ () => handlerClickONG() }>Seja Parceira</button>
+                <button class="profile-btn" onClick={() => handlerClickStudent()}>Inscreva-se agora</button>
               </div>
             </div>
           </div>
 
-          <div>
+          <div class="card">
             <div>
-              <h2>Empresa</h2>
+              <h2 class="card-profile-title">ONG</h2>
+            </div>
+            <div class="img-div">
+              <img src={ong} alt="ong" />
             </div>
             <div>
-              imagem
-            </div>
-            <div>
-              <p>
+              <p class="profile-text">
                 Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
               </p>
               <div>
-                <button onClick={ () => handlerClickCompany() }>Seja Parceira</button>
+                <button class="profile-btn" onClick={() => handlerClickONG()}>Seja Parceira</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="card">
+            <div>
+              <h2 class="card-profile-title">Empresa</h2>
+            </div>
+            <div class="img-div">
+              <img src={empresa} alt="empresa" />
+            </div>
+            <div>
+              <p class="profile-text">
+                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+              </p>
+              <div>
+                <button class="profile-btn" onClick={() => handlerClickCompany()}>Seja Parceira</button>
               </div>
             </div>
           </div>
